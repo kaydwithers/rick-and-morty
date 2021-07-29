2fb9a8b154d59a7a98f142d3db31375dffc4ad6e
Kayd Withers <kaydwithers@gmail.com>
2021-07-29 21:22:42 +1000

    style: Add button styling.

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="onClick">
+  <button>
     <slot />
   </button>
 </template>
@@ -7,20 +7,21 @@
 <script>
 export default {
   name: "Button",
-  props: {
-    onClick: {
-      type: Function,
-      required: true,
-    },
-  },
 };
 </script>
 
 <style scoped>
 button {
   background-color: var(--background-color-accent);
+  border-radius: var(--border-radius);
   color: #000;
   font-size: 1.5rem;
   padding: 1rem 2rem;
+  transition-duration: 300ms;
+  transition-property: transform;
+}
+
+button:hover {
+  transform: scaleX(1.05) scaleY(1.05);
 }
 </style>
